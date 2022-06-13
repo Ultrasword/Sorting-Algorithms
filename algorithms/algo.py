0d88fccf5903b6c6dfca896f9fd2c138572b5e08
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-13 17:25:57 -0400

    added pygame engine

---
+++ algorithms/algo.py
@@ -0,0 +1,8 @@
+
+
+class Algo:
+    def __init__(self):
+        self.dataset = None
+    
+    def update(self):
+        pass
---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+
+WINDOW_AREA = (1280, 720)
+SORT_SIZE = 200
+SORT_WIDTH = WINDOW_AREA[0] // SORT_SIZE
+
+
